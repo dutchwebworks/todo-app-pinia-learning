836e14756a0045fb1863c6c3686046a6f2cbcfe5
Dennis Burger <dennis.burger@poort80.nl>
2022-08-24 14:10:41 +0200

    Initial working todo app with Vue and Pinia

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import { ref } from "vue";
+  import { useTodoListStore } from "../stores/todoList.js";
+
+  const todo = ref("");
+  const store = useTodoListStore();
+
+  function addItemAndClear(item) {
+    if (item.length === 0) {
+      return;
+    }
+
+    store.addTodo(item);
+    todo.value = "";
+  }
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="addItemAndClear(todo)">
+      <input type="text" v-model="todo" />
+      <button>Add</button>
+    </form>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import { useTodoListStore } from "../stores/todoList";
+  import { storeToRefs } from "pinia";
+
+  const store = useTodoListStore();
+  const { todoList } = storeToRefs(store);
+  const { toggleCompleted, deleteTodo } = store;
+</script>
+
+<template>
+  <ul>
+    <li v-for="todo in todoList" :key="todo.id">
+      <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
+      <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+      <span @click.stop="deleteTodo(todo.id)">&#10060;</span>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+  .completed {
+    text-decoration: line-through;
+  }
+</style>
